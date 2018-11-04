e052adc48de86e926506491dc7c63ce00f2966fb
Adam Sajdak <adam.sajdak@onehydra.com>
2018-11-04 23:42:48 +0100

    Initial commit

---
+++ Generator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FullFramework
+{
+    public class Generator
+    {
+        private Random _r = new Random();
+        public string Generate()
+        {
+            return _r.Next().ToString();
+        }
+    }
+}
